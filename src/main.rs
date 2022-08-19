5d97639d2d404db7be511b566244c8b00d107990
Euan Kemp <euank@euank.com>
2022-08-19 00:48:37 -0700

    Add banned letters initial impl

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 use std::io::{self, Write};
 
 use itertools::Itertools;
@@ -15,7 +15,7 @@ fn main() -> Result<(), std::io::Error> {
     let full: HashSet<&str> = DICT_FULL.lines().collect();
 
     let puzzle = find_puzzle(&mut rng, &common, &full);
-    println!("Puzzle: {}", puzzle.blanks);
+    println!("Puzzle: {}, Banned: {}", puzzle.blanks, puzzle.banned.iter().join(", "));
 
 
     loop {
@@ -43,17 +43,63 @@ fn main() -> Result<(), std::io::Error> {
 #[derive(Debug, Clone)]
 struct Puzzle {
     answer: String,
-    banned: String,
+    banned: HashSet<char>,
     blanks: String,
 }
 
 fn find_puzzle<R: Rng>(rng: &mut R, common: &HashSet<&str>, full: &HashSet<&str>) -> Puzzle {
     // otherwise, try to find any viable child path
-    let mut full_minus_common = full.clone();
+    let mut full = full.clone();
     for word in common {
-        full_minus_common.remove(word);
+        full.remove(word);
     }
-    find_sub_puzzle(rng, "", "", 0, common, &full_minus_common).unwrap()
+    let mut puzzle = find_sub_puzzle(rng, "", &HashSet::new(), 0, common, &full).unwrap();
+
+    // trim unessential bans
+    let banned = puzzle.banned.clone();
+    let mut minimal_bans = puzzle.banned.clone();
+    for ban in banned {
+        println!("Trying to remove {}", ban);
+        minimal_bans.remove(&ban);
+        let c = Puzzle{answer: puzzle.answer.clone(), banned: minimal_bans.clone(), blanks: puzzle.blanks.clone()};
+        if is_valid(c.clone(), common, &full) {
+            println!("valid");
+            puzzle = c
+        } else {
+            minimal_bans.insert(ban);
+        }
+    }
+    puzzle
+}
+
+fn exact_matches(word: &str, blanks: &str) -> bool {
+    if word.len() != blanks.len() {
+        return false
+    }
+    let word_chars = word.chars().collect_vec();
+    let blanks_chars = blanks.chars().collect_vec();
+    for i in 0..word_chars.len() {
+        if blanks_chars[i] == '_' {
+            continue
+        }
+        if word_chars[i] != blanks_chars[i] {
+            return false
+        }
+    }
+    true
+}
+
+fn is_valid(p: Puzzle, common: &HashSet<&str>, full: &HashSet<&str>) -> bool {
+    let num_common = common.iter()
+        .filter(|el| exact_matches(el, &p.blanks))
+        .filter(|el| p.banned.is_empty() || !(el.chars().any(|c| p.banned.contains(&c))))
+        .count();
+    let num_full = full.iter()
+        .filter(|el| exact_matches(el, &p.blanks))
+        .filter(|el| p.banned.is_empty() || !(el.chars().any(|c| p.banned.contains(&c))))
+        .count();
+    println!("num common and full: {} {}", num_common, num_full);
+    num_common == 1 && num_full == 0
 }
 
 
@@ -73,9 +119,15 @@ fn prefix_matches(el: &str, prefix: &str) -> bool {
     true
 }
 
-fn find_sub_puzzle<R: Rng>(rng: &mut R, prefix: &str, banned: &str, num_blanks: u64, candidates: &HashSet<&str>, full: &HashSet<&str>) -> Option<Puzzle> {
-    let c: HashSet<&str> = candidates.clone().into_iter().filter(|el| prefix_matches(el, prefix)).collect();
-    let full_matches: HashSet<&str> = full.clone().into_iter().filter(|el| prefix_matches(el, prefix)).collect();
+fn find_sub_puzzle<R: Rng>(rng: &mut R, prefix: &str, banned: &HashSet<char>, num_blanks: u64, candidates: &HashSet<&str>, full: &HashSet<&str>) -> Option<Puzzle> {
+    let c: HashSet<&str> = candidates.clone().into_iter()
+        .filter(|el| prefix_matches(el, prefix))
+        .filter(|el| banned.is_empty() || !(el.chars().any(|c| banned.contains(&c))))
+        .collect();
+    let full_matches: HashSet<&str> = full.clone().into_iter()
+        .filter(|el| prefix_matches(el, prefix))
+        .filter(|el| banned.is_empty() || !(el.chars().any(|c| banned.contains(&c))))
+        .collect();
 
     // failed path, nothing to see here
     if c.is_empty() {
@@ -95,30 +147,60 @@ fn find_sub_puzzle<R: Rng>(rng: &mut R, prefix: &str, banned: &str, num_blanks: 
 
         return Some(Puzzle{
             answer: answer.clone(),
-            banned: banned.to_string(),
+            banned: banned.clone(),
             // prefix, + remaining chars as blanks
             blanks: prefix.to_string() + &(0..(answer.len() - prefix.len())).map(|_| '_').collect::<String>(),
         });
     }
 
     println!("candidates: {}", c.len());
-
-    // otherwise, let's use rng to decide what to do
-    let mut next_options = ('a'..='z').collect_vec();
-    next_options.shuffle(rng);
-    // weight blanks at 30%
-    if rng.gen::<f64>() < 0.3 {
-        next_options.push('_');
-        next_options.rotate_right(1);
-    } else {
-        next_options.rotate_right(rng.gen_range(0..27));
+    // so our next options are:
+    // 1. blank
+    // 2. ban a letter
+    // 3. append a letter
+    //
+    // We want to weight things appropriately. Let's have 30% chance of a blank, 40% of a ban, and
+    // 60% of a letter.
+    // Add all options so we might do any of them of course, but then weight appropriately.
+    #[derive(PartialEq, Clone, Debug)]
+    enum Move {
+        Blank,
+        Ban(char),
+        Append(char),
     }
+    let mut moves = vec![Move::Blank];
+    moves.append(&mut (('a'..='z').map(Move::Ban).collect_vec()));
+    moves.append(&mut (('a'..='z').map(Move::Append).collect_vec()));
 
-    // see if there's any viable puzzles down this path
-    for next in next_options {
-        let next_prefix = prefix.to_owned() + &next.to_string();
-        if let Some(p) = find_sub_puzzle(rng, &next_prefix, &banned, num_blanks + (if next == '_' { 1 } else { 0 }), &c, full) {
-            return Some(p);
+    while !moves.is_empty() {
+        let next_move = moves.choose_weighted(rng, |m| match m {
+            Move::Blank => 30f64,
+            Move::Ban(_) => 100f64 / 26f64,
+            Move::Append(_) => 40f64 / 26f64,
+        }).unwrap().clone();
+        moves.retain(|el| *el != next_move);
+
+        println!("Trying move: {:?}", next_move);
+
+        let p = match next_move {
+            Move::Blank => {
+                find_sub_puzzle(rng, &(prefix.to_owned() + &'_'.to_string()), banned, num_blanks + 1, &c, full)
+            }
+            Move::Ban(chr) => {
+                if banned.contains(&chr) {
+                    continue
+                }
+                let mut subbanned = banned.clone();
+                subbanned.insert(chr);
+                find_sub_puzzle(rng, prefix, &subbanned, num_blanks, &c, full)
+            }
+            Move::Append(chr) => {
+                find_sub_puzzle(rng, &(prefix.to_owned() + &chr.to_string()), banned, num_blanks, &c, full)
+            }
+        };
+
+        if p.is_some() {
+            return p
         }
     }
     None
